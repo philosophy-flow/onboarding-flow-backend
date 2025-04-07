6b4202f19ccd39a1ca8b30b35babee5e1f6ff0ff
Jacob Stewart <jacob.g.stewart@icloud.com>
2025-04-06 22:10:41 -0400

    remove api prefix

--- app/main.py
+++ app/main.py
@@ -17,9 +17,9 @@ allowed_origins = (
 app = FastAPI(
     title="Onboarding Flow API",
     version="1.0.0",
-    openapi_url="/api/openapi.json",
-    docs_url="/api/docs",
-    redoc_url="/api/redoc",
+    openapi_url="/openapi.json",
+    docs_url="/docs",
+    redoc_url="/redoc",
 )
 
 app.add_middleware(
@@ -36,5 +36,9 @@ def read_root():
     return {"message": "Service is up."}
 
 
-app.include_router(user.router, prefix="/api")
-app.include_router(page.router, prefix="/api")
+app.include_router(
+    user.router,
+)
+app.include_router(
+    page.router,
+)
